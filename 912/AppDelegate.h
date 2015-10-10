8ce2d3a7741682fbb03d3625273d862259ad02f8
Arron <Arron@ArrondeMacBook-Pro.local>
2015-10-11 00:20:05 +0800

    Initial Commit

---
+++ 912/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  912
+//
+//  Created by Arron on 10/11/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 912/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  912
+//
+//  Created by Arron on 10/11/15.
+//  Copyright (c) 2015 Arron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
